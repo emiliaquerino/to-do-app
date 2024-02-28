3c34b25e9857f65a1065b9c5549eed458c5c3be3
Emília Querino <emiliaquerino@gmail.com>
2024-02-28 08:41:21 -0300

    Add e configura o bg da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Box, Flex, Text, useColorMode } from "@chakra-ui/react";
+import lightBackgroundImage from "./assets/bg-desktop-light.jpg";
+import darkBackgroundImage from "./assets/bg-desktop-dark.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { colorMode, toggleColorMode } = useColorMode();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return  <>
+    <Box
+     backgroundImage={
+      colorMode === "light" ? lightBackgroundImage : darkBackgroundImage
+    }
+    backgroundSize={"cover"}
+    h={"40vh"}
+  >
+      <Box w={{ base: "80%", md: "60%", lg: "40%" }} p="4em 0" m="auto">
+          <Header colorMode={colorMode} toggleColorMode={toggleColorMode} />
+          {/* <InputButton
+            colorMode={colorMode}
+            todo={todo}
+            setTodo={setTodo}
+            addTodo={handleAddTodo}
+          /> */}
+      </Box>
+    </Box>
+    </>;
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import App from './App.jsx'
-import './index.css'
+
+import App from "./App.jsx";
 
 export const theme = extendTheme({
   fonts: {
